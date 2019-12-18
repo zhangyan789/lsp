e2f9f93ce2e3898a6fbce9adc923e69fc3fcf44a
li17324910702 <1161838951@qq.com>
2019-12-18 12:01:35 +0800

    李鑫鑫 李永祥 3.2

---
+++ app/src/main/java/com/wulianwang/lsp/activity/Itemview.java
@@ -0,0 +1,26 @@
+package com.wulianwang.lsp.activity;
+
+public class Itemview {
+    private  String time;
+    private  String itmename;
+    private  String content;
+
+    public Itemview(){
+    }
+    public Itemview(String time,String itmename,String content){
+        this.time=time;
+        this.content=content;
+        this.itmename=itmename;
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
