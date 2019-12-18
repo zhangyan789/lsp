a3cbe26523be84af393e1e0ed75bce215431b514
lyy1005664097 <1005664097@qq.com>
2019-12-18 23:00:27 +0800

    电力服务跳转

---
+++ app/src/main/java/com/wulianwang/lsp/activity/MyWorkListActivity.java
@@ -0,0 +1,34 @@
+package com.wulianwang.lsp.activity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.wulianwang.lsp.R;
+
+public class MyWorkListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_service);
+
+        RelativeLayout tv1 = (RelativeLayout) findViewById(R.id.person);
+        RelativeLayout tv2 = (RelativeLayout) findViewById(R.id.company);
+
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        tv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+}
